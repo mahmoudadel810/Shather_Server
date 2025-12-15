0630b5527cbfd57740ea577273d9731dcf30ea96
m7amedez5511-hue <m7amedez5511@gmail.com>
2025-12-15 10:35:33 +0200

    Revert "chore: initialize project"
    This reverts commit 5efc9fe84ca9fbe87e9f65613b749fa019c7926d.

--- Utils/generateUniqueString.js
+++
@@ -1,7 +0,0 @@
-import {  customAlphabet } from 'nanoid';
-
-
-export const generateUniqueString = (length) => {
-    const nanoid = customAlphabet('mohamedez573', length);
-    return nanoid();
-}
--- middleware/errorHangling.js
+++
@@ -1,9 +0,0 @@
-export const errorHandling = (error, req, res, next) => {
-  if (error) {
-    return res.status(error['cause'] || 500).json({
-      success: false,
-      message: error?.message || "Internal Server Error",
-      stack: error.stack,
-    })
-  }
-}
